3317268bf1d8b668d43a28430282fdcde35e29f1
Riazul Islam <riazul5207@gmail.com>
2024-07-10 09:38:20 +0600

    navbar section updated

--- src/layouts/main-layout/navbar/index.tsx
+++ src/layouts/main-layout/navbar/index.tsx
@@ -1,24 +1,47 @@
-import { Box, AppBar, Toolbar, Button, Typography } from '@mui/material';
+import { Box, AppBar, Toolbar, Button, Typography, IconButton, Stack } from '@mui/material';
+import IconifyIcon from 'components/base/IconifyIcon';
 
 const Navbar = () => {
   return (
     <Box width={1} bgcolor="info.dark" zIndex={1200}>
-      <AppBar component="nav" sx={{position: 'relative', mx: "auto", px: 1.5, py: 0.5, maxWidth: 1320, bgcolor: 'transparent', boxShadow: 'none'}}>
+      <AppBar
+        component="nav"
+        sx={{
+          position: 'relative',
+          mx: 'auto',
+          px: 1.5,
+          py: 0.5,
+          maxWidth: 1320,
+          bgcolor: 'transparent',
+          boxShadow: 'none',
+        }}
+      >
         <Toolbar>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
-            <Button sx={{ color: '#fff' }}>Home</Button>
-            <Button sx={{ color: '#fff' }}>Shop</Button>
-            <Button sx={{ color: '#fff' }}>Blog</Button>
-            <Button sx={{ color: '#fff' }}>About Us</Button>
-            <Button sx={{ color: '#fff' }}>Contact Us</Button>
+            <Button sx={{ color: '#fff', fontSize: 'body1.fontSize', fontWeight: 500 }}>
+              Home
+            </Button>
+            <Button sx={{ color: '#fff', fontSize: 'body1.fontSize', fontWeight: 500 }}>
+              Shop
+            </Button>
+            <Button sx={{ color: '#fff', fontSize: 'body1.fontSize', fontWeight: 500 }}>
+              Blog
+            </Button>
+            <Button sx={{ color: '#fff', fontSize: 'body1.fontSize', fontWeight: 500 }}>
+              About Us
+            </Button>
+            <Button sx={{ color: '#fff', fontSize: 'body1.fontSize', fontWeight: 500 }}>
+              Contact Us
+            </Button>
           </Box>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-          >
-            Phone
-          </Typography>
+          <Stack spacing={0.25} alignItems="center">
+            <IconButton size="large" sx={{ backgroundColor: 'transparent !important' }}>
+              <IconifyIcon icon="ph:phone-call" color="info.lighter" />
+            </IconButton>
+            <Typography variant="body1" color="info.lighter" fontWeight={500}>
+              (219) 555-0114
+            </Typography>
+          </Stack>
         </Toolbar>
       </AppBar>
     </Box>
