3f149d0f6e7cd1b26edd2c1dfc912d58123369c7
Riazul Islam <riazul5207@gmail.com>
2024-07-13 11:26:15 +0600

    updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import Box from '@mui/material/Box';
-// import Hero from 'components/sections/hero';
-// import FeaturedServices from 'components/sections/featured-services';
+import Hero from 'components/sections/hero';
+import FeaturedServices from 'components/sections/featured-services';
 import PopularCategories from 'components/sections/popular-categories';
 
 const Home = () => {
   return (
     <>
       <Box mx="auto" px={1.5} width={1} maxWidth={1332}>
-        {/* <Hero />
-        <FeaturedServices /> */}
+        <Hero />
+        <FeaturedServices />
         <PopularCategories/>
       </Box>
 
