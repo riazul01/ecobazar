09592d9f749217f1a95679d241f47f9e65682065
riazul islam <riazul.codes@gmail.com>
2026-08-18 14:49:54 +0600

    feat: add change password section

--- src/pages/account/Settings.tsx
+++ src/pages/account/Settings.tsx
@@ -1,12 +1,14 @@
 import Stack from "@mui/material/Stack";
 import AccountSettings from "components/sections/account/settings/AccountSettings";
 import BillingAddress from "components/sections/account/settings/BillingAddress";
+import ChangePassword from "components/sections/account/settings/ChangePassword";
 
 const Settings = () => {
   return (
-    <Stack direction="column" sx={{ gap: 3, p: 3, width: 1 }}>
+    <Stack direction="column" spacing={3} sx={{ width: 1 }}>
       <AccountSettings />
       <BillingAddress />
+      <ChangePassword />
     </Stack>
   );
 };
